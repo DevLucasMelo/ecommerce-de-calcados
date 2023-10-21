b89409397462382b0eabdc085b58b39795e79254
Lucas Faustini de Melo <faustinilucas8@gmail.com>
2023-10-21 08:23:55 -0300

    Devolucao view e controller

---
+++ EcommerceBack/Controllers/DevolucaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceBack.Controllers
+{
+    public class DevolucaoController : Controller
+    {
+        public IActionResult Devolucao()
+        {
+            return View();
+        }
+    }
+}
